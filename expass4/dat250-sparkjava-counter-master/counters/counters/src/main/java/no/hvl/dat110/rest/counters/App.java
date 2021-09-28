9d1bcda5b8a8f97211f6be0a6c6e900a6ee41bb7
Tuva <tuvakvalsoren@gmail.com>
2021-09-28 17:37:23 +0200

    Add expass4

---
+++ expass4/dat250-sparkjava-counter-master/counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
@@ -0,0 +1,53 @@
+package no.hvl.dat110.rest.counters;
+
+import static spark.Spark.after;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.put;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	static Counters counters = null;
+	
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			port(Integer.parseInt(args[0]));
+		} else {
+			port(8080);
+		}
+
+		counters = new Counters();
+		
+		after((req, res) -> {
+  		  res.type("application/json");
+  		});
+		
+		get("/hello", (req, res) -> "Hello World!");
+		
+        get("/counters", (req, res) -> counters.toJson());
+ 
+        get("/counters/red", (req, res) -> counters.getRed());
+
+        get("/counters/green", (req, res) -> counters.getGreen());
+
+        // TODO: put for green/red and in JSON
+        // variant that returns link/references to red and green counter
+        put("/counters", (req,res) -> {
+        
+        	Gson gson = new Gson();
+        	
+        	counters = gson.fromJson(req.body(), Counters.class);
+        
+            return counters.toJson();
+        	
+        });
+    }
+    
+}
